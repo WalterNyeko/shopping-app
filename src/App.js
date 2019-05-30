bab34602386b7fbcaea8a6f40b60fdb351e6fe5e
JSnakegitHub <nyekowalter69@gmail.com>
2019-05-30 15:22:47 +0300

    feat(login and signup): add login and sign up modals
    - adds login modal
    - adds signup modal
    - adds navbar logic for login
    
    [finishes #]

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Routes from './helpers/Routes';
 import LandingPage from './containers/home/LandingPage';
+import SignupPage from './containers/customers/SignupPage';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <BrowserRouter>
           <Switch>
             <Route path={Routes.landingPage} exact component={LandingPage}/>
+            <Route path={Routes.signupPage} exact component={SignupPage}/>
 
           </Switch>
         </BrowserRouter>
